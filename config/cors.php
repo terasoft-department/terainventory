ad5069097a7dfe99c0832a9c6ba32e4fc7294bf8
mvc <nyemamudhihir@gmail.com>
2024-09-24 17:51:31 +0300

    second commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','https://7tbf3bgr-5173.uks1.devtunnels.ms'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
